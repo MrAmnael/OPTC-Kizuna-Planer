5ba534e4d84a7343f74322e293142aaec3bff7ad
MrAmnael <clevan150@gmail.com>
2026-04-19 19:49:16 +0200

    Update boss_stats.js

--- js/boss_stats.js
+++ js/boss_stats.js
@@ -8,30 +8,30 @@ window.bossStats = {
     // ==========================================
     
     "STR": {
-        "Boss": "Ulti & Page One",
+        "Boss": "",
         "Classes": "",
-        "Personnages Recommandés": "[4518][4519]",
-        "Stage 1": "Pour les [Slasher] / [Striker] / [Cerebral] :\n [Spe CD]Temps de chargement des coups spéciaux réduit de 15 tours.\n [Switch CD]Nombre requis avant l`activation des Super effets de changements réduit de 15 tours.\n [VS CD]Réduit la jauge des effets VS de 15 tours.\n [Retraite]Retraite!",
-        "Stage 2": "[Actions Preventives]\n [ATK Chain Lock]Pendant 2 tour(s), si le multiplicateur de combo est inférieur ou égal a 2,5, Réduit l`ATK de 99%. \n Pendant 7 tour(s) Augmentation des chances d'apparation des cercles [DEX]\n Pendant 7 tour(s) Rend les cercles [DEX]désavantageux.\n [Normal Only]Dégats autres que ceux des ATK normales réduits à 1.\n Changement aléatoire de cercles.\n [Def UP]Pendant 6 tour(s) DEF x 1000000.\n [Berserker]Pendant 98 tour(s) Augmentation de l'ATK, Réduction du nombre de tours d'action.\n\n [Interruption] Multiplicateur de combo bloqué.\n Jusqu'a 1 fois.\n [Spe Retour]Inversion du temps de chargement des coups spéciaux de 3 tour(s).\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n\n [Actions Vaincu]\n [Heal]Personnage : Restaure les HP jusqu'a 100%.\n [Damage]80000 points de dégats.\n [Retraite]Retraite!",
-        "Stage 3 (Lv31+)": "Apparition de 6 ennemi(s)\n [Etat Initial]Ulti et Page One\n [Immunités]Immunisé aux altérations d'état hormis Retard.\n [Immunité Death]Immunise contre les Spéciaux de mort immédiate.\n [Résistance dégats] Résistance aux dégats proportionnels 100%\n Classe faible [FreeSpirit]\n\n [Actions Preventives]\n [Chain Down] Pendant 7 tour(s) Hausse du multiplicateur de combo X 0.1.\n [Intimidate] Pendant 3 tour(s) parmi les altérations concernés, réduit certaines altérations d'état de 1 tour(s) (Altérations concernés : Augmentation de l'ATK de l'équipe / Blocage de multiplicateur de combo / Augmentations des effets de type des ATK)\n Pendant 3 tour(s) Applique 'Territoire Ennemi' sur le terrain.\n Dégats a la fin du tour des alliés : Annulation (1tour(s)).\n Coéquipiers : Inversion du temps de chargement des coups spéciaux de 1 tour(s).\n [Normal Only]Dégats autres que ceux des ATK normales réduits a 1.\n [ATK Down]Pendant 4 tour(s), lors d'ATK avec des cercles [QCK][WA][SEMLA][Arc-en-ciel][RCV][TND], réduit les dégâts infligés de 99%.\n [Resilience]Pendant 10 tour(s) Résistance avec 1 HP.\n [Poison]Pendant 98 tour(s) Poison progressifdont les dégâts augmentent avec le temps.\n\n [Interruption]Losque les alliés sont en état de [Normal Only] n'est pas appliqué (aprés l'activation de Coup spécial, Super, effet VS, Effet de changement / Effet de super changement) :\n Nombre de fois illimitées.\n Restaure tous les HP.\n Pendant 99 tour(s) : Silence.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n [Normal Only]Dégats autres que ceux des ATK normales réduits à 1.\n\n [Interruption]Losque les alliés sont en état de [ATK Down] n'est pas appliqué (aprés l'activation de Coup spécial, Super, effet VS, Effet de changement / Effet de super changement).\n Jusqu'à 1 fois.\n [Dommage +]Pendant 10 tour(s) Dégâts subis par l'équipe X 2.\n [Berserker]Inflige à un ennemi spécifique des dégats équivalents à 100% des HP max de la cible.\n\n [Interruption] Retards\n Jusqu'à 1 fois.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n [Berserker] Pendant 10 tour(s) Augmentation de l'ATK, Réduction du nombre de tours d'action.\n\n [Interruption]Dégâts supplémentaires à la fin du tour.\n Nombre de fois illimitées.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n\n [Interruption]Combustion \n Nombre de foi illimitées.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés."
+        "Personnages Recommandés": "",
+        "Stage 1": "",
+        "Stage 2": "",
+        "Stage 3 (Lv31+)": ""
     },
 
     "DEX": {
-        "Boss": "",
-        "Classes": "",
-        "Personnages Recommandés": "",
-        "Stage 1": "Pour les  :\n- CD de 15 tours\n- Super Switch de 15 tours\n- VS Gauge de 15 tours",
-        "Stage 2": "",
-        "Stage 3 (Lv31+)": ""
+        "Boss": "Eustass Kid",
+        "Classes": "Intellectuel",
+        "Personnages Recommandés": "[4547][4548]",
+        "Stage 1": "Pour les [Cogneur] / [Tireur] / [Intellectuel] :\n [Spe CD]Réduction du temps de chargement des coups spéciaux de 15 tours.\n [Switch CD]Chargement du Super changement de 15 tours.\n [VS CD]Jauge VS de 15 tours.\n [Retraite]Retraite !",
+        "Stage 2": "Apparition de 5 ennemis.\n\n [Actions Preventives]\n [Spe Retour]Inversion des coups spéciaux des coéquipiers (2 tours).\n [Spe Bloqué]Coup spécial bloqué des coéquipiers (PERFECT x2).\n [Barriere]Barrière de timing (GREAT x2).\n [Normal Only]Dégâts autres que ceux des ATK normales réduits à 1.\n\n [Interruption]Après 1 tour :\n [Slot Bloqué]Lien des cercles (3 tours)\n [Retraite]Retraite.",
+        "Stage 3 (Lv31+)": "[Etat Initial] Eustass Kid.\n [Immunités]Immunisé aux altérations d'état (sauf Retard).\n [Immunité Death]Immunisé aux Spéciaux de mort immédiate.\n [Résistance dégats]Résistance aux dégâts proportionnels 100%.\n Classe vulnérable : [Intellectuel].\n\n [Actions Preventives]\n [SP Bind]Lien des coups spéciaux des coéquipiers (10 tours).\n [ATK Down]ATK de l'équipe x1.1 (3 tours).\n [Def UP]DEF +10 000 000 (6 tours).\n Applique 'Territoire Ennemi' sur le terrain (3 tours).\n [Limites]Lors d'ATK avec des cercles [DEX][QCK][PSY][INT][WA][RCV][TND][Bomb], réduit les dégâts de 90%.\n [Intimidate]Réduit la durée de la réduction de dégâts / seuil de l'équipe de 2 tours.\n [Normal Only]Dégâts autres que ceux des ATK normales réduits à 1.\n\n [Interruption]Si [Normal Only]n'est pas appliqué (après Spécial / Super / VS / Changement) :\n [Heal]Restaure tous les HP,\n [Silence]Silence (99 tours),\n [Clear]Supprime les bonus,\n [Normal Only]NAO.\n\n [Actions Vaincu]\n [Damage]220 000 points de dégâts."
     },
 
     "QCK": {
-        "Boss": "",
-        "Classes": "",
-        "Personnages Recommandés": "",
-        "Stage 1": "Pour les  :\n- CD de 15 tours\n- Super Switch de 15 tours\n- VS Gauge de 15 tours",
-        "Stage 2": "",
-        "Stage 3 (Lv31+)": ""
+        "Boss": "Eustass Kid",
+        "Classes": "Intellectuel",
+        "Personnages Recommandés": "[4547][4548]",
+        "Stage 1": "Pour les [STR] / [DEX] :\n [Spe CD]Réduction du temps de chargement des coups spéciaux de 15 tours.\n [Switch CD]Chargement du Super changement de 15 tours.\n [VS CD]Jauge VS de 15 tours.\n [Retraite]Retraite !",
+        "Stage 2": "[BLK]Change tous les cercles en [BLK].\n [SP Bind]Lien des coups spéciaux (6 tours).\n [Barriere]Barrière de cercles ([STR] x2, pendant 3 tours).\n [Normal Only]Dégâts autres que ceux des ATK normales réduits à 1.\n [Intimidate] Pendant 2 tour(s), parmi les altérations d'état concernées, réduit certaines altérations d'état de 1 tour (Altération d'état concernées : Augmentation de l'ATK de l'équipe).",
+        "Stage 3 (Lv31+)": "[Etat Initial] Eustass Kid.\n [Immunités]Immunisé aux altérations d'état (sauf [Poison]).\n [Immunité Death]Immunisé aux Spéciaux de mort immédiate.\n [Résistance dégats]Résistance aux dégâts proportionnels 100%.\n Classe vulnérable : [Intellectuel].\n\n [Actions Preventives]\n [SP Bind]Lien des coups spéciaux (7 tours).\n [Despair]Désespoir des capitaines (10 tours).\n [Slot Lock]Bloque les cercles (1 tour).\n [Limites]Cercles [DEX][QCK][PSY][INT][TND] désavantageux (2 tours).\n L'équipe ne peut infliger que 80% des HP max de l'ennemi par tour (10 tours).\n Applique 'Territoire Ennemi' (3 tours).\n [Damage]162 000 points de dégâts.\n [Despair]Désespoir des coéquipiers (1 tour).\n [Resilience]Résistance avec 1 HP (6 tours).\n [Intimidate] Pendant 3 tour(s), parmi les altérations d'état concernées, réduit certaines altérations d'état de 1 tour (Altération d'état concernées : Augmentation de l'ATK de l'équipe).\n [Normal Only]Dégâts autres que ceux des ATK normales réduits à 1.\n\n [Actions Normal]\n [Hunger]Jusqu'à l'obtention de 3 cercles [RCV]/[SEMLA]consommés : Réduit les HP de l'équipe de 30% chaque tour et [ATK Down]ATK -90%.\n\n [Interruption]Si [Normal Only]n'est pas appliqué (après Spécial / Super / VS / Changement) :\n Restaure tous les HP,\n [Silence]Silence (99 tours),\n [Clear]Supprime les bonus,\n [Normal Only]NAO."
     },
 
     "PSY": {
@@ -44,12 +44,12 @@ window.bossStats = {
     },
 
     "INT": {
-        "Boss": "Ulti & Page One",
+        "Boss": "",
         "Classes": "",
-        "Personnages Recommandés": "[4518][4519]",
-        "Stage 1": "Pour les [STR] / [DEX] / [PSY] :\n [Spe CD]Temps de chargement des coups spéciaux réduit de 15 tours.\n [Switch CD]Nombre requis avant l`activation des Super effets de changements réduit de 15 tours.\n [VS CD]Réduit la jauge des effets VS de 15 tours.\n [Retraite]Retraite!",
-        "Stage 2": "Apparition de 6 ennemi(s)\n [Etat Initial]Page One\n [Immunité Retard]Immunité : Retard.\n Pendant 4 tour(s) Barrière de cercles ([WA]1 fois).\n\n [Actions Preventives]\n Changement des cercles [EMP][STR][DEX][QCK][PSY][INT][G][RCV][TND][BOMB] en [Cercle Poison]\n [Resilience]Pendant 6 tour(s) Résistance avec 1HP.\n\n [Actions Vaincu]\n Personnage : Restaure les HP jusqu'à 100%.\n [Normal Only] Dégâts autres que ceux des ATK normales réduits à 1.\n [Retraite]Retraite.\n\n [Interruption] Après 3 tour(s)\n [Paralysie]Pendant 2 tour(s) Paralysie.\n [Retraite]Retraite.\n\n [Interruption] Après 3 tour(s)\n [Paralysie]Pendant 2 tour(s) Paralysie.\n [Retraite]Retraite.\n\n [Interruption] Après 3 tour(s)\n [Paralysie]Pendant 2 tour(s) Paralysie.\n [Retraite]Retraite.\n\n [Interruption] Après 3 tour(s)\n [Paralysie]Pendant 2 tour(s) Paralysie.\n [Retraite]Retraite.\n\n [Interruption] Après 3 tour(s)\n [Paralysie]Pendant 2 tour(s) Paralysie.\n [Retraite]Retraite.",
-        "Stage 3 (Lv31+)": "[Etat Initial] Ulti et Page One.\n [Immunités]Immunité aux altérations d'état hormis Réduction de DES des ennemis.\n [Immunité Death]Immunise contres les Spéciaux de mort immédiate.\n [Résistance dégats]Résistance aux dégâts proportionnels 100%.\n Classe faible : [FreeSpirit]\n\n [Actions Preventives]\n [Normal Only]Dégâts autres que ceux des ATK normales réduits à 1.\n [Def UP]Pendant 6 tour(s) DEF X 1000000.\n [Treshold]Pendant 6 tour(s) Dégâts supérieurs à 1000 réduit de 90%.\n [ATK Down]Pendant 1 tour(s), lors d'ATK avec des cercles [WA][SEMLA][Arc-en-ciel], réduit les dégâts infligés de 99%.\n [Pain]Pendant 3 tour(s), 20000 points de dégâts reçus après une action qui n'est pas engendrée par un coup normal.\n Pendant 3 tour(s) Rend les cercles [PSY][INT][RCV][TND]désavantageux.\n [Intimidate] Pendant 3 tour(s) parmi les altérations concernés, réduit certaines altérations d'état de 2 tour(s) (Altérations concernés : Augmentation de l'ATK de l'équipe)\n\n [Interruption]Losque les alliés sont en état de [Normal Only] n'est pas appliqué (aprés l'activation de Coup spécial, Super, effet VS, Effet de changement / Effet de super changement) :\n Nombre de fois illimitées.\n Restaure tous les HP.\n Pendant 99 tour(s) : Silence.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n [Normal Only]Dégats autres que ceux des ATK normales réduits à 1.\n\n [Interruption]Augmentation de l'ATK\n Jusqu'à 2 fois.\n [Clear]Effets bénéfique (dont Etat Super) / Sommes cummulées annulés.\n\n [Interruption]Changement de cercles\n Jusqu'à 1 fois.\n Changement de cercles [EMP][STR][DEX][QCK][PSY][INT][G][RCV][TND][BOMB] en [BLK].\n\n [Actions Vaincu]\n [Damage]150000 points de dégâts."
+        "Personnages Recommandés": "",
+        "Stage 1": "",
+        "Stage 2": "",
+        "Stage 3 (Lv31+)": ""
     },
 
     // ==========================================
